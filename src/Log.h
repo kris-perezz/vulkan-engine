ff27172702638a4fc22fc98c39c0469ba714b7d7
kris-perezz <kris.ilijay.perez@gmail.com>
2025-10-10 07:54:47 -0600

    feat: added new ASSERT_LOG macro

--- src/Log.h
+++ src/Log.h
@@ -46,5 +46,15 @@ class Log {
 #define LOG_INFO(...) PEREZLOG::Log::info(__VA_ARGS__)   // spdlog::info(fmt)
 #define LOG_WARN(...) PEREZLOG::Log::warn(__VA_ARGS__)   // spdlog::warn(fmt)
 #define LOG_ERROR(...) PEREZLOG::Log::error(__VA_ARGS__)    // spdlog::error(__VA_ARGS__)
-#define ZEN_LOG_CRITICAL(...) PEREZLOG::Log::critical(__VA_ARGS__) // spdlog::critical(fmt)
+#define LOG_CRITICAL(...) PEREZLOG::Log::critical(__VA_ARGS__) // spdlog::critical(fmt)
+
+#define ASSERT_LOG(cond, msg, ...)                                                       \
+    do {                                                                                 \
+        if (!(cond)) {                                                                   \
+            LOG_CRITICAL("Assertion failed:\n  expr : {}\n  msg  : " msg "\n  loc  : {}:{}" \
+                      , #cond, ##__VA_ARGS__, __FILE__, __LINE__);                       \
+            std::abort();                                                                \
+        }                                                                                \
+    } while (0)
+
 }; // namespace PEREZLOG
