91fe1816fdf914617723febfd78fc488da16ce42
kris-perezz <kris.ilijay.perez@gmail.com>
2025-10-10 07:56:14 -0600

    feat: implemented createGraphicsPipeline

--- src/Pipeline.h
+++ src/Pipeline.h
@@ -6,14 +6,28 @@
 #include <vulkan/vulkan_core.h>
 
 namespace kopi {
-  struct PipelineConfigInfo {};
+  struct PipelineConfigInfo {
+    VkViewport viewport;
+    VkRect2D scissor;
+    VkPipelineViewportStateCreateInfo viewportInfo;
+    VkPipelineInputAssemblyStateCreateInfo inputAssemblyInfo;
+    VkPipelineRasterizationStateCreateInfo rasterizationInfo;
+    VkPipelineMultisampleStateCreateInfo multisampleInfo;
+    VkPipelineColorBlendAttachmentState colorBlendAttachment;
+    VkPipelineColorBlendStateCreateInfo colorBlendInfo;
+    VkPipelineDepthStencilStateCreateInfo depthStencilInfo;
+    VkPipelineLayout pipelineLayout = nullptr;
+    VkRenderPass renderPass         = nullptr;
+    uint32_t subpass                = 0;
+  };
   class Pipeline {
   public:
     Pipeline(EngineDevice &device,
              const std::string &vertFilePath,
              const std::string &fragFilePath,
              const PipelineConfigInfo &configInfo);
-    ~Pipeline() {}
+             
+    ~Pipeline();
 
     Pipeline(const Pipeline &)       = delete;
     void operator=(const Pipeline &) = delete;
